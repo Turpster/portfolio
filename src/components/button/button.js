d672d5823d7ee1a473becb84876cfa72c2eb405e
Turpster <turpster@turpster.net>
2020-03-04 01:40:17 +0000

    Added prop 'onClick' function to Button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./button.css"
 
+import PropTypes from 'prop-types'
+
 export default class Button extends React.Component
 {
+    static propTypes = {
+        onClick: PropTypes.func
+    };
+
+    static defaultProps = {
+        onClick: undefined
+    };
+
     render() {
         return (
-            <input type={"button"} className={"button"} value={this.props.children}/>
+            <input type={"button"} className={"button"} value={this.props.children} onClick={this.props.onClick}/>
         )
     }
 }
