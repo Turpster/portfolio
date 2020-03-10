4ed1ae2996f87c30c44bead98063d4e433a484d4
Turpster <turpster@turpster.net>
2020-03-10 23:03:30 +0000

    Added props to side-nav.js

--- src/components/side-nav/side-nav.js
+++ src/components/side-nav/side-nav.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import "./side-nav.css"
 
+import PropTypes from 'prop-types'
+
 export default class SideNav extends React.Component
 {
+    static propTypes = {
+        yMargin: PropTypes.string,
+        yDelay: PropTypes.string
+    };
+
+    static defaultProps = {
+        yMargin: undefined,
+        yDelay: undefined
+    };
+
     render() {
         return (
-            <ul className={"side-nav"}>
+            <ul className={"side-nav"} style={{"--nav-bar-y-margin": this.props.yMargin, "--nav-bar-y-delay": this.props.yDelay}}>
                 <h1>Portfolio</h1>
                 <li><a href={"#about"}>About</a></li>
                 <li><a href={"#projects"}>Projects</a></li>
