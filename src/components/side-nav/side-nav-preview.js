7ce6b99ce6cec0f3d7bb68e91ec51a30fb0f6af2
Turpster <turpster@turpster.net>
2020-03-10 22:42:43 +0000

    Created side-nav preview

---
+++ src/components/side-nav/side-nav-preview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SideNav from "./side-nav";
+
+export default class SideNavPreview extends React.Component
+{
+    render() {
+        return (
+            <div style={{"height": "600px", "border": "2px dashed black", "margin": "10px"}}>
+                <SideNav/>
+            </div>
+        )
+    }
+}
